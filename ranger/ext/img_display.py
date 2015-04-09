c1300fa5f2210d6db438e1b435145c5155e3d969
fey <feyrinen@gmail.com>
2015-04-09 12:48:41 +0200

    fix encode .ora file path before hashing

--- ranger/ext/img_display.py
+++ ranger/ext/img_display.py
@@ -96,7 +96,7 @@ class W3MImageDisplayer(ImageDisplayer):
         if os.path.splitext(path)[1][1:].lower() == "ora":
             now = datetime.datetime.now()
             ago = now - datetime.timedelta(hours=1)
-            tmpFilename = "/tmp/" + hashlib.md5(path).hexdigest() + ".png"
+            tmpFilename = "/tmp/" + hashlib.md5(path.encode('utf-8')).hexdigest() + ".png"
             if os.path.isfile(tmpFilename) and datetime.datetime.fromtimestamp(os.stat(tmpFilename).st_mtime) > ago:
                 pass
             else:
